6db2c4fd5ac1d21ceb5bc631b21806d177c6ca8d
Alan Piovezan <alan.log.piovezan@gmail.com>
2020-08-07 12:28:16 -0300

    Cricao Endpoint /cliente/{id}

---
+++ src/main/java/br/com/alan/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package br.com.alan.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alan.cursomc.domain.Cliente;
+import br.com.alan.cursomc.repositories.ClienteRepository;
+import br.com.alan.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscarPorId(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));	}
+}
