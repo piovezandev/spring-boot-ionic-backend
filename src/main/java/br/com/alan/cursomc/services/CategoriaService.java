6d795c699e53e00ea2df9c06c00fc5d8857715f1
Alan Piovezan <alan.log.piovezan@gmail.com>
2020-08-21 21:02:46 -0300

    paginação com parametros na requisicao

--- src/main/java/br/com/alan/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/alan/cursomc/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.jaxb.SpringDataJaxb.PageRequestDto;
 import org.springframework.stereotype.Service;
 
 import br.com.alan.cursomc.domain.Categoria;
@@ -44,5 +48,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
+		
 	}
 }
